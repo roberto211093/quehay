60ad011877e4a7191323db4341fc2958ccf73c12
Rafael Acosta <rafael.acosta@toctoc.com>
2023-05-15 04:44:23 -0400

    upgrade react native from 0.62.2 to 0.71.8

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
+export const API_KEY = 'cdef3bdcce3546a61c9f6a3d135f244a';
+export const LANG = 'es-VE';
+export const API_HOST = 'https://api.themoviedb.org/3';
